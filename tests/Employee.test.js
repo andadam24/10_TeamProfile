8307291b4926fbe8ab063e196e54c2a1805ce0c4
Adam And <andadam24@gmail.com>
2021-09-05 15:16:28 -0400

    updated employee test

---
+++ tests/Employee.test.js
@@ -0,0 +1,40 @@
+const Employee = require ("../lib/Employee")
+
+describe("Employee class", () => {
+    it("Should create an employee class with employee name, email, id", () => {
+      const employee = new Employee("Bart",100,"bart.simpson@test.com" );
+
+      expect(employee.name).toEqual("Bart");
+      expect(employee.id).toEqual("100");
+      expect(employee.email).toEqual("bart.simpson@test.com");
+      
+    });
+  
+    describe("getName", () => {
+      it("should return employee's name",()=> {
+        const employee = new employee("Bart",100,"bart.simpson@test.com" );
+        expect(employee.name).toEqual("Bart");
+      })
+    });
+  
+    describe("getEmail", () => {
+        it("should return the employee's email",()=> {
+          const employee = new employee("Bart",100,"bart.simpson@test.com" );
+          expect(employee.email).toEqual("bart.simpson@test.com");
+        })
+      });
+    
+      describe("getID", () => {
+        it("should return the employee's ID",()=> {
+          const employee = new employee("Bart",100,"bart.simpson@test.com" );
+          expect(employee.id).toEqual("100");
+        })
+      });
+
+      describe("getRole", () => {
+        it("should return the employee's role",()=> {
+          const employee = new employee("Bart",100,"bart.simpson@test.com" );
+          expect(employee.getRole()).toEqual("Employee");
+        })
+      });
+  });
